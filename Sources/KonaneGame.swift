981c84cb983ae9f4d81554e362d9c11a996b350b
Apple61752 <mohamedapple@hotmail.com>
2016-11-03 03:37:36 +0000

    Add files back in

---
+++ Sources/KonaneGame.swift
@@ -0,0 +1,16 @@
+game = KonaneGame(blackIsHuman: true, whiteIsHuman: true)
+class KonaneGame {
+    init(blackIsHuman: Bool, whiteIsHuman: Bool) {
+        }
+    }
+    func play() -> Bool {
+        return didBlackWin
+    }
+var didBlackWin: Bool = false
+var didWhiteWin: Bool = false
+
+    private var gameState: KonaneGameState
+    private var blackInputSource: KonaneMoveInputSource
+    private var whiteInputSource: KonaneMoveInputSource
+    private func displayBoard() {
+    }
---
+++ Sources/KonaneMove.swift
@@ -0,0 +1,12 @@
+class KonaneMove {
+
+}
+
+var gameBoardPieces: [Character] = []
+for i in 0..<16 {
+    if true {
+        gameBoardPieces.append("o")
+    } else {
+        gameBoardPieces.append("x")
+    }
+}
